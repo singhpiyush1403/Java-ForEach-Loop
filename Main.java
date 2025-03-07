fc913677711e0565a1ee1fa5090eaa75a8553f2d
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-07 13:23:57 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class ForEachExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int num : numbers) {  
+            System.out.println("Number: " + num);
+        }
+    }
+}
